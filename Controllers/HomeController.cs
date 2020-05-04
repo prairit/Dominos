82a28fa96a2e361f40aebf533480f9f3bd1347ea
prairit <54899228+prairit@users.noreply.github.com>
2020-05-04 21:20:06 +0530

    Added Menu page and Checkout page along with a common layout

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,10 +13,20 @@ namespace Doms.Controllers
             return View();
         }
 
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
             return View();
         }
 
